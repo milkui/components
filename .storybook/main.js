4a9d45c0e11c29682d5d980e6ce2858581cbb2ae
Matias Trujillo <mtrujillo@outlook.cl>
2022-04-30 06:58:06 -0400

    add webpack 5 support (#1)

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,4 +2,7 @@ module.exports = {
   framework: '@storybook/html',
   stories: ['../packages/core/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: 'webpack5',
+  },
 };
